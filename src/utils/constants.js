8feb1df15cf854fd3f4e9c9a6ec15d23f52ea11a
evansibok <evansibok@gmail.com>
2021-10-13 16:14:44 +0100

    my first nft app

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const TWITTER_HANDLE = 'evansibok';
+export const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+export const OPENSEA_LINK = 'https://testnets.opensea.io';
+export const OPENSEA_COLLECTION = 'senseinft-ivhkaulqms';
+export const TOTAL_MINT_COUNT = 50;
+export const CONTRACT_ADDRESS = '0xEf8b1292088e47d492651f52EEf6A867b21427DD';
+export const RINKEBY_CHAIN_ID = "0x4";
